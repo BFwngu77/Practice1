eeb5b5a3d89cba92913ea5efa49e669b3289462e
Brett Foreman <bfcollege7@yahoo.com>
2016-12-19 12:25:50 -0800

    first rev of Practice 1 BF

--- Practice 1/ViewController.swift
+++ Practice 1/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var enterMessageLabel1: UILabel!
+    
+    @IBOutlet weak var enterMessageLabel2: UILabel!
+    
+    @IBOutlet weak var enterNameTextBox: UITextField!
+    
+    @IBOutlet weak var enterMessageTextBox: UITextField!
+    
+    @IBOutlet weak var enterButtonTextBox: UIButton!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
-
+    
+    
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        enterMessageLabel2.hidden = false
+        enterMessageLabel2.text = enterMessageTextBox.text
+        enterMessageLabel2.textColor = UIColor.blueColor()
+        
+        enterMessageTextBox.text = ""
+        enterMessageTextBox.resignFirstResponder()
+        
+        enterButtonTextBox.setTitle("Button Pressed", forState: UIControlState.Normal)
+    }
+    
+    
+    }
